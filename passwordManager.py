e1e2f780b13485bad3ee7976b77a13187355c52a
I3WX <utkarshs7828@gmail.com>
2024-06-23 16:04:28 +0530

    All function added succesfuly adn ther are working fine

--- passwordManager.py
+++ passwordManager.py
@@ -1,4 +1,4 @@
-from sys import platform
+from turtle import update
 from cryptography.fernet import Fernet
 import mysql.connector
 
@@ -23,6 +23,12 @@ def encrypt_password(password, key):
     return encrypted_password
 
 
+def decrypt_password(encrypted_password, key):
+    cipher_suite = Fernet(key=key)
+    password = cipher_suite.decrypt(encrypted_password).decode()
+    return password
+
+
 def get_data_from_user():
     print("Enter your Details")
     username = str(input("Enter your username: "))
@@ -40,17 +46,24 @@ def display_available_platform():
     return individual_platforms
 
 
-def get_platform():
-    individual_platforms = display_available_platform()
-    platform = str(input("Enter your platform (a to add a new platform) : "))
-    if platform in individual_platforms:
-        return platform
-    elif platform == "a":
-        new_platform = input("Enter name of new platform : ")
-        query = f"CREATE TABLE {new_platform} (No INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(64), password BLOB, encryption_key BLOB );"
-        cursor.execute(query)
-        print("new Platform Added")
-        return new_platform
+def get_platform(allow_new_platform_input=True):
+    while True:
+        individual_platforms = display_available_platform()
+        if allow_new_platform_input:
+            platform = str(input("Enter your platform (a to add a new platform): "))
+        else:
+            platform = str(input("Enter your platform: "))
+
+        if platform in individual_platforms:
+            return platform
+        elif allow_new_platform_input and platform == "a":
+            new_platform = input("Enter name of new platform : ")
+            query = f"CREATE TABLE {new_platform} (No INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(64), password BLOB, encryption_key BLOB );"
+            cursor.execute(query)
+            print("New Platform Added")
+            return new_platform
+        else:
+            print("Error in choosing try again...")
 
 
 def save_data(username, password, platform):
@@ -61,6 +74,44 @@ def save_data(username, password, platform):
     cursor.execute(query, data)
     conn.commit()
     print(f"User {username} and password successfully saved for {platform}.")
+
+
+def get_user_data(username, platform):
+    query = ("SELECT password, encryption_key FROM {} WHERE username=%s;").format(
+        platform
+    )
+    cursor.execute(query, (username,))
+    result = cursor.fetchone()
+    if result:
+        encrypted_password, key = result
+        password = decrypt_password(
+            encrypted_password=encrypted_password.decode(), key=key.decode()
+        )
+        return password
+
+
+def update_password(username, old_password, new_password, platform):
+    password = get_user_data(username=username, platform=platform)
+    if password == old_password:
+        key = get_key()
+        encrypted_new_password = encrypt_password(password=new_password, key=key)
+        query = (
+            f"UPDATE {platform} SET password=%s, encryption_key=%s WHERE username=%s"
+        )
+        cursor.execute(query, (encrypted_new_password, key, username))
+        conn.commit()
+        print(f"User {username} and password successfully updated for {platform}.")
+
+
+def Delete_password(username, password, platform):
+    password = get_user_data(username=username, platform=platform)
+    if password == password:
+        query = f"DELETE FROM {platform} WHERE username=%s"
+        cursor.execute(query, (username,))
+        conn.commit()
+        print(f"Password for {username} deleted successfully.")
+    else:
+        print("Password didn't match with exiting database")
 
 
 def display_menu():
@@ -81,12 +132,42 @@ def main():
             platform = get_platform()
             save_data(username=username, password=password, platform=platform)
             print(f"Username and password saved successfully in {platform}.")
+            q = input("Enter q to quit else press any button : ")
+            if q == "q":
+                break
+            else:
+                continue
         elif x == 2:
-            print("Check password feature is not implemented yet.")
+            platform = get_platform(allow_new_platform_input=False)
+            username = str(input("Enter your username: "))
+            password = get_user_data(username, platform)
+            print(f"Password of {username} in {platform} is \n {password}")
+            q = input("Enter q to quit else press any button : ")
+            if q == "q":
+                break
+            else:
+                continue
         elif x == 3:
-            print("Update password feature is not implemented yet.")
+            platform = get_platform(allow_new_platform_input=False)
+            username = str(input("Enter your username: "))
+            old_password = str(input("Enter old password: "))
+            new_password = str(input("Enter new password: "))
+            update_password(username, old_password, new_password, platform)
+            q = input("Enter q to quit else press any button : ")
+            if q == "q":
+                break
+            else:
+                continue
         elif x == 4:
-            print("Delete password feature is not implemented yet.")
+            platform = get_platform(allow_new_platform_input=False)
+            username = str(input("Enter your username: "))
+            password = str(input("Enter old password: "))
+            Delete_password(username=username, password=password, platform=platform)
+            q = input("Enter q to quit else press any button : ")
+            if q == "q":
+                break
+            else:
+                continue
         elif x == 5:
             break
 
